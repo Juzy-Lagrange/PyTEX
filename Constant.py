4e065177ecaa5a4971d77d3a5c00ab65505621b0
Juzy-Lagrange <ntdpy.dame.py@gmail.com>
2022-10-20 00:27:12 +0500

    constant0.1

---
+++ Constant.py
@@ -0,0 +1,28 @@
+#example
+
+def epsilon():
+	print(" \\varepsilon ")
+
+def nothing():
+	print(" \\varnothing ")
+
+def cap():
+	print(" \\cap ")
+
+def cup():
+	print(" \\cup ")
+
+def nl():
+	print("\\\\")
+
+def circ():
+	print(" \\circ ")
+
+def tms():
+	print(" \\times ")
+
+def triangle():
+	print(" \\triangle ")
+
+
+if __name__ == "__main__":
